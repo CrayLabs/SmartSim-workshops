afcbf6b2d02e5e49ddb9cbadef3940741ba0ff93
Al Rigazzi <al.rigazzi@hpe.com>
2023-02-15 11:23:31 +0100

    Add fallback for make_swan.sh scripts

--- 20230215/solutions/src/fortran/make_app_swan.sh
+++ 20230215/solutions/src/fortran/make_app_swan.sh
@@ -4,6 +4,7 @@ module unload atp craype-broadwell
 
 # Use generic target to be able to run on any x86_64 CPU
 export CRAY_CPU_TARGET=x86-64
+SMARTREDIS_INSTALL_PATH="${SMARTREDIS_INSTALL_PATH:-${SMARTREDIS_LIBRARY}}"
 
 mkdir build
 cd build
